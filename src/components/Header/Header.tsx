8768d564a91f81281a713379b7c48219663188f6
Yosber Guerrero <yosbergc@gmail.com>
2025-02-06 07:25:41 -0400

    Added conditional rendering depending of width, hidden modal for languages for mobile and desktop, added new style to make translate responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,28 @@
 import { LanguageConstant } from "../../types/types";
 import './Header.css'
 import { TranslateContext } from "../../contexts/TranslateContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 function Header({ isFromLanguage, languages, currentSelected } : { isFromLanguage: boolean, languages: LanguageConstant[], currentSelected: string}) {
-
     const context = useContext(TranslateContext)
-    
+    const [width, setWidth] = useState(window.innerWidth)
+    const [showLanguages, setShowLanguages] = useState(false)
+    function handleWidth() {
+        setWidth(window.innerWidth)
+    }
+    useEffect(() => {
+        window.addEventListener('resize', handleWidth)
+
+        return () => {
+            window.removeEventListener('resize', handleWidth)
+        }
+    })
     return (
         <section className="header-translate">
             {
-                isFromLanguage && <button className={currentSelected === 'auto' ? 'language-button active' : 'language-button'} onClick={() => {context?.changeFromLanguage('auto')}}>Detectar idioma</button>
+                width > 768 && isFromLanguage && <button className={currentSelected === 'auto' ? 'language-button active' : 'language-button'} onClick={() => {context?.changeFromLanguage('auto')}}>Detectar idioma</button>
             }
             {
-                languages.map((language, index) => {
+                width > 768 && languages.map((language, index) => {
                     if (index > 2) return;
                     return <button 
                     key={language.code}
@@ -27,7 +37,62 @@ function Header({ isFromLanguage, languages, currentSelected } : { isFromLanguag
                     >{language.name}</button>
                 })
             }
-            <button className="down-arrow"></button>
+            {
+                width > 768 && <section className="other-languages">
+                    <button className="down-arrow" onClick={() => setShowLanguages(!showLanguages)}></button>
+                    {
+                        showLanguages && <section className="hiddenLanguages">
+                            {
+                                languages.map((language, index) => {
+                                    if (index < 3) return;
+                                    return <button 
+                                    key={language.code}
+                                    className={currentSelected === language.code ? 'language-button active' : 'language-button'}
+                                    onClick={() => {
+                                        if (isFromLanguage) {
+                                            context?.changeFromLanguage(language.code)
+                                        } else {
+                                            context?.changeToLanguage(language.code)
+                                        }
+                                    }}
+                                    >{language.name}</button>
+                                })
+                            }
+                        </section>
+                    }
+                </section>
+            }
+            {
+                width < 768 && languages.filter(language => language.code === currentSelected).map((language) => {
+                    return <section className="other-languages">
+                        <button 
+                        key={language.code}
+                        className={currentSelected === language.code ? 'language-button active' : 'language-button'}
+                        onClick={() => setShowLanguages(!showLanguages)}
+                        >{language.name}</button>
+                        {
+                        showLanguages && <section className="hiddenLanguages">
+                            {
+                                languages.map((language) => {
+                                    return <button 
+                                    key={language.code}
+                                    className={currentSelected === language.code ? 'language-button active' : 'language-button'}
+                                    onClick={() => {
+                                        setShowLanguages(false)
+                                        if (isFromLanguage) {
+                                            context?.changeFromLanguage(language.code)
+                                        } else {
+                                            context?.changeToLanguage(language.code)
+                                        }
+                                    }}
+                                    >{language.name}</button>
+                                })
+                            }
+                        </section>
+                    }
+                    </section>
+                })
+            }
         </section>
     )
 }
